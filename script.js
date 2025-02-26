2aceb5e559af30f8a745f9961f2976664219f258
susmitamenka <susmitaprasad73@gmail.com>
2025-02-26 17:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 //your JS code here. If required.
-	
-function removeColor(Select and Remove){
-	let dropdown = document.getElementById("colorSelect");
-	let selectedIndex = dropdown.selectedIndex;
-if (selectedIndex !== -1 ){
-	dropdown.remove(selectedIndex)
-}
-
-
-}
+function removeColor() {
+            const colorSelect = document.getElementById("colorSelect");
+            colorSelect.remove(colorSelect.selectedIndex);
+        }
