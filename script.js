c506de9c6d911361c591c54830e9bdbcf9860dfe
susmitamenka <susmitaprasad73@gmail.com>
2025-04-23 15:11:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //your JS code here. If required.
-function removeColor() {
-            const colorSelect = document.getElementById("colorSelect");
-            colorSelect.remove(colorSelect.selectedIndex);
-        }
+// function removeColor() {
+//             const colorSelect = document.getElementById("colorSelect");
+//             colorSelect.remove(colorSelect.selectedIndex);
+//         }
+let  dropdown = document.getElementById("colorSelect");
+for (let  i = 0; i < dropdown.options.length; i++) {
+    if (dropdown.options[i].value === "green") {
+        dropdown.remove(i);
+        break; // Stop after removing
+    }
+}
